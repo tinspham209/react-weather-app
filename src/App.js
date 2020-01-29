0cf30cdafbca3306b090a43bd8701dd983c51c77
tinspham <tindeptrai1@gmail.com>
2020-01-29 11:55:29 +0700

    Updated App.js Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import AppLayout from "./containers/AppLayout/AppLayout";
+import Layout from "./hoc/Layout/Layout";
 function App() {
   return (
     <div>
-      <AppLayout />
+      <Layout>
+        <AppLayout />
+      </Layout>
     </div>
   );
 }
