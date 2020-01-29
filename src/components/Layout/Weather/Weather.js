059468548696d03ae71a5abff8378b7c01692e3d
tinspham <tindeptrai1@gmail.com>
2020-01-29 11:53:43 +0700

    Updated Weather Component

--- src/components/Layout/Weather/Weather.js
+++ src/components/Layout/Weather/Weather.js
@@ -4,8 +4,11 @@ import * as weatherIcons from "../../../database/icons.json";
 import * as recommendations from "../../../database/recommendations.json";
 const weather = props => {
   let temp = props.currentWeather.main.temp;
+
   let location = props.currentWeather.name;
+
   let country = props.currentWeather.sys.country;
+
   let dt = props.currentWeather.dt;
   let date = new Date(dt * 1000);
   let hour = date.getHours();
@@ -20,17 +23,22 @@ const weather = props => {
   weekday[5] = "Friday";
   weekday[6] = "Saturday";
   let day = weekday[date.getDay()];
+
   let description = props.currentWeather.weather[0].description;
-  let uppderCaseDescription =
+  let upperCaseDescription =
     description.charAt(0).toUpperCase() + description.slice(1);
+  
   let windSpeed = Math.floor(props.currentWeather.wind.speed * 3.6);
+  
   let humidity = props.currentWeather.main.humidity;
 
+  //get className of icon
   const prefix = "wi wi-";
   const icon =
     prefix + weatherIcons.default[props.currentWeather.weather[0].id].icon;
   const recommendation =
     recommendations.default[props.currentWeather.weather[0].id].recommendation;
+
   return (
     <div className={classes.Weather}>
       <div className={classes.WeatherHeader}>
@@ -38,7 +46,7 @@ const weather = props => {
           {location}, {country}
         </span>
         <span>
-          {day}, {formattedTime}, {uppderCaseDescription}
+          {day}, {formattedTime}, {upperCaseDescription}
         </span>
       </div>
 
@@ -59,13 +67,11 @@ const weather = props => {
           </h4>
         </div>
         <div className={classes.col}>
-          {/* <div className={"wi wi-day-sunny-overcast jss286"}></div> */}
           <div className={icon} style={{ color: "purple" }}></div>
         </div>
       </div>
       <div className={classes.sologan}>
         <p>
-          {/* Great day for a bit of laundry and maybe a nice picnic date later :) */}
           {recommendation}
         </p>
       </div>
