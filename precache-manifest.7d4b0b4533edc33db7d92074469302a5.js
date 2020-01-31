63366c397e3627654abcef07347ffd83c9919013
tinspham <tindeptrai1@gmail.com>
2020-01-31 22:28:35 +0700

    Updates

---
+++ precache-manifest.7d4b0b4533edc33db7d92074469302a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c451a1631f6c5fb8f2e370c9fdb0a4ce",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "e0a1baeca4abae6662e3",
+    "url": "/react-weather-app/static/css/main.e0186368.chunk.css"
+  },
+  {
+    "revision": "e309fd54262c563a43a6",
+    "url": "/react-weather-app/static/js/2.5f0b5347.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-weather-app/static/js/2.5f0b5347.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e0a1baeca4abae6662e3",
+    "url": "/react-weather-app/static/js/main.cbf5cb14.chunk.js"
+  },
+  {
+    "revision": "d2857d5f4a409e252419",
+    "url": "/react-weather-app/static/js/runtime-main.a75341be.js"
+  },
+  {
+    "revision": "fa2da4e6b61aa745f198314584dd8826",
+    "url": "/react-weather-app/static/media/search.fa2da4e6.png"
+  }
+]);
