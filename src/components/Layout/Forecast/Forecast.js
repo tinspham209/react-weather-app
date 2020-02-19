ad87ec180e3a2bcda85aeb7577ec50863fb78d47
tinspham <tindeptrai1@gmail.com>
2020-02-19 20:42:07 +0700

    Adding React-dnd to Forecast

--- src/components/Layout/Forecast/Forecast.js
+++ src/components/Layout/Forecast/Forecast.js
@@ -1,32 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ForecastPerDay from "./ForecastPerDay/ForecastPerDay";
 import classes from "./Forecast.css";
-import {getWeekday, getDate} from '../../../shared/utility';
-const forecast = props => {
+import { DndProvider } from "react-dnd";
+import HTML5Backend from "react-dnd-html5-backend";
 
-  let forecastPerDay = Object.keys(props.currentForecast).map(index => {
-    return [...Array(props.currentForecast[index])].map(object => {
-      const daytime = object.dt;
-      const date = getDate(daytime);
-      
-      return (
-        <li key={index}>
-          <ForecastPerDay
-            day={getWeekday(date)}
-            lowTemp={object.main.temp_min}
-            highTemp={object.main.temp_max}
-            id={object.weather[0].id}
-          />
-        </li>
-      );
-    });
+import { getWeekday, getDate } from "../../../shared/utility";
+const Forecast = props => {
+  // let foreCastSelected = forecast.find(foreCastSelected => foreCastPerDay.idSource === idSource);
+  const [forecast, setForecast] = useState(props.currentForecast);
+  console.log('forecast', forecast);
+  useEffect(() => {
+    setForecast(props.currentForecast);
+  }, [props.currentForecast]);
+  const onDrop = (idItemDrag, idItemDrop) => {
+    const indexDrag = forecast.findIndex( item => item.dt === idItemDrag );
+    const indexDrop = forecast.findIndex( item => item.dt === idItemDrop );
+
+    let newForecast = [...forecast];
+    const forecastDrag = newForecast[indexDrag];
+    console.log('newForecast', newForecast);
+    for(let index = indexDrag; index < indexDrop; index++){
+      newForecast[index] = newForecast[index+1];
+    }
+    newForecast[indexDrop] = forecastDrag;
+    setForecast(newForecast);
+  };
+  let forecastPerDay = forecast.map(object => {
+    const daytime = object.dt;
+    const date = getDate(daytime);
+
+    return (
+      <li key={object.dt}>
+        <ForecastPerDay
+          day={getWeekday(date)}
+          lowTemp={object.main.temp_min}
+          highTemp={object.main.temp_max}
+          id={object.weather[0].id}
+          idSource={object.dt}
+          onDrop= {onDrop}
+        />
+      </li>
+    );
   });
 
   return (
     <div className={classes.Forecast}>
-      <ul>{forecastPerDay}</ul>
+      <DndProvider backend={HTML5Backend}>
+        <ul>{forecastPerDay}</ul>
+      </DndProvider>
     </div>
   );
 };
 
-export default forecast;
+export default Forecast;
--- src/components/Layout/Forecast/ForecastPerDay/ForecastPerDay.js
+++ src/components/Layout/Forecast/ForecastPerDay/ForecastPerDay.js
@@ -1,23 +1,46 @@
-import React from "react";
+import React, {useRef} from "react";
 import classes from "./ForecastPerDay.css";
-import * as weatherIcons from "../../../../database/icons.json"
-import {getIcon} from '../../../../shared/utility';
-const forecastPerDay = props => {
+import { useDrag, useDrop } from "react-dnd";
+import * as weatherIcons from "../../../../database/icons.json";
+import { getIcon } from "../../../../shared/utility";
+
+const ForecastPerDay = props => {
   const lowTemp = Math.floor(props.lowTemp);
   const highTemp = Math.floor(props.highTemp);
-  
+
   const icon = getIcon(weatherIcons.default[props.id]);
+  const id = props.idSource;
+  const ref = useRef(null);
+  const [{ isDragging }, drag] = useDrag({
+    item: {type: "card", id},
+    collect: monitor => ({
+      isDragging: monitor.isDragging()
+    })
+  });
+  const opacity = isDragging ? 0.5 : 1;
+  const [, drop] = useDrop({
+    accept: "card",
+    drop(item, monitor) {
+      console.log('item', item.id);
+      props.onDrop(item.id, props.idSource);
+    }
+  });
+  
+  drop(ref);
+  drag(ref);
+
+
 
   return (
-    <div className={classes.ForecastPerDay}>
+    <div ref={ref} style={{opacity}} className={classes.ForecastPerDay} >
       <div className={classes.row}>
-        <div className={classes.col} style={{flexGrow: '4'}}>
+        <div className={classes.col} style={{ flexGrow: "4" }}>
           <span>{props.day}</span>
         </div>
-        <div className={classes.col} style={{flexGrow: '2'}}>
-          <div className={icon} style={{color: 'purple'}} ></div>
+        <div className={classes.col} style={{ flexGrow: "2" }}>
+          <div className={icon} style={{ color: "purple" }}></div>
         </div>
-        <div className={classes.col} style={{flexGrow: '4'}}>
+        <div className={classes.col} style={{ flexGrow: "4" }}>
           <span>
             {lowTemp}&deg; / {highTemp}&deg;
           </span>
@@ -27,4 +50,4 @@ const forecastPerDay = props => {
   );
 };
 
-export default forecastPerDay;
+export default ForecastPerDay;
